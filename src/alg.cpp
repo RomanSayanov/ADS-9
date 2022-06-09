a228ec423a0b6b492233ca2755ffdb0cb1286f2d
RomanSayanov <94217225+RomanSayanov@users.noreply.github.com>
2022-06-10 00:46:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,8 +3,46 @@
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
+#include <string>
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+    BST<std::string> tree;
+    std::ifstream fin(filename);
+    bool flag = 0;
+    int count = 0;
+    while (!flag) {
+        std::string word;
+        std::string temp;
+        std::getline(fin, temp);
+        if (temp == "") {
+            count++;
+            if (count == 20) {
+                flag = 1;
+                break;
+            }
+            continue;
+        }
+        count = 0;
+        for (int i = 0; i < temp.size(); i++) {
+            if (temp[i] <= 'Z' && temp[i] >= 'A' || temp[i] <= 'z' && temp[i] >= 'a') {
+                if (temp[i] <= 'Z' && temp[i] >= 'A') {
+                    word += temp[i] + ('a' - 'A');
+                }
+                if (temp[i] <= 'z' && temp[i] >= 'a') {
+                    word += temp[i];
+                }
+                if (i == temp.size() - 1) {
+                    tree.add(word);
+                }
+            } else if (temp[i] == ' ') {
+                tree.add(word);
+                word = "";
+            } else {
+                tree.add(word);
+                word = "";
+            }
+        }
+    }
+    return tree;
 }
